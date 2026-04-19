e7c652efa23d731955e9efaeef05c5c38c582235
ym46797 <120094182+ym46797@users.noreply.github.com>
2026-04-19 18:42:08 -0400

    Initial commit: Setup correct repository structure and script

---
+++ adls_practice/list_contents.py
@@ -0,0 +1,22 @@
+from azure.storage.blob import BlobServiceClient
+
+def main():
+    # Define the blob service URL
+    service_url = "https://azurepublicdatasettraces.blob.core.windows.net/"
+
+    # Create a BlobServiceClient
+    blob_service_client = BlobServiceClient(account_url=service_url)
+
+    # Define the container name
+    container_name = "azurepublicdataset"
+
+    # List all blobs in the container
+    container_client = blob_service_client.get_container_client(container_name)
+
+    print(f"Listing files in container that end in .csv.gz : {container_name}")
+    for blob in container_client.list_blobs():
+       if blob.name.endswith('.csv.gz'):
+            print(blob.name)
+
+if __name__ == "__main__":
+    main()
